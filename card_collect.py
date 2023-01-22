a578cda94ade29a8ea6ab7b7858b5dd06092580d
k-shcherb <48098278+k-shcherb@users.noreply.github.com>
2023-01-22 14:46:44 -0500

    updated card_capture script for 1080p

--- card_collect.py
+++ card_collect.py
@@ -1,9 +1,6 @@
-import pygetwindow
-import pyautogui
-import win32gui, win32com.client
+import win32gui
 from PIL import ImageGrab
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 
 toplist, winlist = [], []
@@ -12,28 +9,28 @@ def enum_cb(hwnd, results):
 win32gui.EnumWindows(enum_cb, toplist)
 yixian = [(hwnd, title) for hwnd, title in winlist if '弈仙牌' in title]
 # win32gui.SetForegroundWindow(yixian[0][0])
-bbox = win32gui.GetWindowRect(yixian[0][0])
-img = np.array(ImageGrab.grab(bbox).convert('RGB'))
 
 q = ""
 card_num = 0
 while q != "quit":
+    bbox = win32gui.GetWindowRect(yixian[0][0])
     img = np.array(ImageGrab.grab(bbox).convert('RGB'))
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    _, thresh = cv2.threshold(gray, 90, 255, type=cv2.THRESH_BINARY_INV)
+    _, thresh = cv2.threshold(gray, 90, 255, type=cv2.THRESH_OTSU)
+    thresh = cv2.dilate(thresh, np.ones((5,5)), iterations=2)
     contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     contours = contours[0] if len(contours) == 2 else contours[1]
 
     cont_cards = []
     for i in contours:
         x, y, w, h = cv2.boundingRect(i)
-        if 180000 > w*h > 60000:
+        if 2000000 > w*h > 100000:
             cont_cards.append(i)
         
     for cnt, i in enumerate(cont_cards):
         x, y, w, h = cv2.boundingRect(i) 
         card1 = gray[y:y+h, x:x+w]
-        cv2.imwrite(f"cards/card_{card_num}.png", cv2.cvtColor(card1, cv2.COLOR_GRAY2BGR))
+        cv2.imwrite(f"cards/card_{card_num}.png", cv2.cvtColor(img[y:y+h,x:x+w], cv2.COLOR_BGR2RGB))
         card_num += 1
     
     q = input("type quit to quit, otherwise hit enter to take image: ")
